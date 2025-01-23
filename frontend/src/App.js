ab41d1fc3d178fbd2d700ac9aca380ea1a5a8b89
johnson <johnsonchan.jc@gmail.com>
2025-01-23 15:05:41 +1100

    cicd github deployment file for test execution

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Modified outside container2 .. what ... Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
